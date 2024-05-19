a524d66303b7c509e3628174968e907668acf883
Geuthur <761682+Geuthur@users.noreply.github.com>
2024-05-19 03:21:48 +0200

    Refactor Template API

--- ledger/api/templatemanager.py
+++ ledger/api/templatemanager.py
@@ -31,6 +31,7 @@ class TemplateFilterCore:
         self.filter_ess = self.filter & Q(ref_type="ess_escrow_transfer")
         self.filter_mining = Q(character__eve_character__character_id__in=self.char_id)
 
+
 class TemplateFilterCost(TemplateFilterCore):
     """TemplateFilter class to store the filter data."""
 
@@ -64,6 +65,7 @@ class TemplateFilterTrading(TemplateFilterCost):
             amount__gt=0,
         )
         self.filter_donation = self.filter & Q(ref_type="player_donation")
+
 
 class TemplateFilter(TemplateFilterTrading):
     """TemplateFilterAll class to store all filter data."""
