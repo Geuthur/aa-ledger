3a90ee17fcc5e5b0e535d55f1fe4177b90ba9929
Geuthur <geuthur@users.noreply.github.com>
2024-05-22 13:19:21 +0200

    Formatting issues

--- ledger/templatetags/ledger.py
+++ ledger/templatetags/ledger.py
@@ -1,5 +1,6 @@
 from django.template.defaulttags import register
 
+
 @register.filter(name="ledger_init")
 def ledger_init():
     return None
--- ledger/views/pve.py
+++ ledger/views/pve.py
@@ -1,20 +1,21 @@
 """PvE Views"""
 
 # Django
+from django.conf import settings
 from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import render
-from django.conf import settings
 
 # Voices of War
 from ledger.hooks import get_extension_logger
 
 logger = get_extension_logger(__name__)
 
+
 @login_required
 @permission_required("ledger.basic_access")
 def ledger_index(request):
     context = {
-        'memberaudit': settings.LEDGER_MEMBERAUDIT_USE,
+        "memberaudit": settings.LEDGER_MEMBERAUDIT_USE,
     }
     return render(request, "ledger/index.html", context=context)
 
@@ -23,7 +24,7 @@ def ledger_index(request):
 @permission_required("ledger.basic_access")
 def ratting_index(request):
     context = {
-        'memberaudit': settings.LEDGER_MEMBERAUDIT_USE,
+        "memberaudit": settings.LEDGER_MEMBERAUDIT_USE,
     }
     return render(request, "ledger/corpledger/corp_ledger.html", context=context)
 
@@ -32,6 +33,6 @@ def ratting_index(request):
 @permission_required("ledger.basic_access")
 def ratting_char_index(request):
     context = {
-        'memberaudit': settings.LEDGER_MEMBERAUDIT_USE,
+        "memberaudit": settings.LEDGER_MEMBERAUDIT_USE,
     }
     return render(request, "ledger/charledger/char_ledger.html", context=context)
