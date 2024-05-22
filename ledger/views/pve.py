5466fffc3950b91802951baa71aae74d62bf5c36
Geuthur <geuthur@users.noreply.github.com>
2024-05-22 13:17:17 +0200

    Added Memberaudit check to disable "Add Char" button

--- ledger/views/pve.py
+++ ledger/views/pve.py
@@ -3,33 +3,35 @@
 # Django
 from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import render
+from django.conf import settings
 
 # Voices of War
 from ledger.hooks import get_extension_logger
 
 logger = get_extension_logger(__name__)
 
-
-@login_required
-@permission_required("ledger.basic_access")
-def index_pve(request):
-    context = {"text": ""}
-    return render(request, "ledger/pve/index.html", context)
-
-
 @login_required
 @permission_required("ledger.basic_access")
 def ledger_index(request):
-    return render(request, "ledger/ledger/index.html")
+    context = {
+        'memberaudit': settings.LEDGER_MEMBERAUDIT_USE,
+    }
+    return render(request, "ledger/index.html", context=context)
 
 
 @login_required
 @permission_required("ledger.basic_access")
 def ratting_index(request):
-    return render(request, "ledger/ledger/corpledger/corp_ledger.html")
+    context = {
+        'memberaudit': settings.LEDGER_MEMBERAUDIT_USE,
+    }
+    return render(request, "ledger/corpledger/corp_ledger.html", context=context)
 
 
 @login_required
 @permission_required("ledger.basic_access")
 def ratting_char_index(request):
-    return render(request, "ledger/ledger/charledger/char_ledger.html")
+    context = {
+        'memberaudit': settings.LEDGER_MEMBERAUDIT_USE,
+    }
+    return render(request, "ledger/charledger/char_ledger.html", context=context)
