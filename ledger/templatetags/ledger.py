4826331eabafc71babbf16e7c517ba17e529f78c
Geuthur <geuthur@users.noreply.github.com>
2024-05-22 11:41:17 +0200

    removed templatetags and used djangos

--- ledger/templatetags/ledger.py
+++ ledger/templatetags/ledger.py
@@ -1,27 +1,18 @@
-from django import template
 from django.template.defaulttags import register
 
-from datetime import datetime
 
-register = template.Library()
-
-@register.filter(name='format_currency')
+@register.filter(name="format_currency")
 def format_currency(value):
     try:
-        value = '{:,.0f}'.format(value).replace(',', '.')
-    except:
+        value = f"{value:,.0f}".replace(",", ".")
+    except ValueError:
         value = 0
     return value
 
-@register.filter(name='current_month')
-def current_month(value):
-    return datetime.now().strftime("%B")
 
-@register.filter(name='current_year')
-def current_year(value):
-    return datetime.now().strftime("%Y")
-
-@register.filter(name='portrait')
+@register.filter(name="portrait")
 def portrait(character_id):
-    portrait = f"https://images.evetech.net/characters/{character_id}/portrait?size=32"
-    return portrait
+    portait_img = (
+        f"https://images.evetech.net/characters/{character_id}/portrait?size=32"
+    )
+    return portait_img
