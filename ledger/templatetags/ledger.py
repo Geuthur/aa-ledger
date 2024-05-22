d0342e7129ae94c920e45cb792a63d38cb3267e6
Geuthur <geuthur@users.noreply.github.com>
2024-05-22 13:16:50 +0200

    removed all functions and create init one

--- ledger/templatetags/ledger.py
+++ ledger/templatetags/ledger.py
@@ -1,18 +1,5 @@
 from django.template.defaulttags import register
 
-
-@register.filter(name="format_currency")
-def format_currency(value):
-    try:
-        value = f"{value:,.0f}".replace(",", ".")
-    except ValueError:
-        value = 0
-    return value
-
-
-@register.filter(name="portrait")
-def portrait(character_id):
-    portait_img = (
-        f"https://images.evetech.net/characters/{character_id}/portrait?size=32"
-    )
-    return portait_img
+@register.filter(name="ledger_init")
+def ledger_init():
+    return None
