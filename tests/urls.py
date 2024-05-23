7f02ae1c57e4d8cc895cb9eb79db7b1a16ee418d
Geuthur <geuthur@users.noreply.github.com>
2024-05-23 08:35:21 +0200

    #

--- tests/urls.py
+++ tests/urls.py
@@ -9,4 +9,5 @@ urlpatterns = allianceauth.urls.urlpatterns
 urlpatterns += [
     # Navhelper test urls
     path("test-page/", views.page, name="p1"),
+    path("test-view/", views.test_settings, name="test_settings"),
 ]
