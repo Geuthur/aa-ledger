d65f3d2beb166b20d756528969c1798bcc74e34e
Geuthur <geuthur@users.noreply.github.com>
2024-05-23 08:23:54 +0200

    Add Testview for settings

--- tests/views.py
+++ tests/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.conf import settings
 
 
 def page(request):  # pylint: disable=unused-argument
     return HttpResponse("Hello I am the Test!")
+
+def test_settings(request):
+    context = {
+        "memberaudit": settings.LEDGER_MEMBERAUDIT_USE,
+    }
+    return render(request, "ledger/corpledger/corp_ledger.html", context=context)
