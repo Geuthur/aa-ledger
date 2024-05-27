75c40b1130dd38d147fe03cf28ac5f8fffec2f85
Geuthur <761682+Geuthur@users.noreply.github.com>
2024-05-27 20:03:39 +0200

    #

--- tests/urls.py
+++ tests/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 
 import allianceauth.urls
 
-from . import test_views, views
+from . import views
 
 urlpatterns = allianceauth.urls.urlpatterns
 
 urlpatterns += [
     # Navhelper test urls
     path("test-page/", views.page, name="p1"),
-    path("test-view/", test_views.test_view, name="test_view"),
 ]
