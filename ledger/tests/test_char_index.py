8858149f1531cf4d3f329b1930025494a570601f
Geuthur <761682+Geuthur@users.noreply.github.com>
2024-05-27 23:24:33 +0200

    #

--- ledger/tests/test_char_index.py
+++ ledger/tests/test_char_index.py
@@ -3,16 +3,28 @@ from unittest.mock import Mock, patch
 from django.contrib.auth.models import AnonymousUser
 from django.test import RequestFactory, TestCase
 
+from app_utils.testdata_factories import UserMainFactory
+
 from ledger.views.character.char_audit import add_char, fetch_memberaudit
 
 
 class AddCharTest(TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserMainFactory(
+            permissions=[
+                "ledger.basic_access",
+            ]
+        )
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
         # Erstellen Sie eine Anfrage und ein Token Mock-Objekt
-        self.factory = RequestFactory()
-        self.request = self.factory.get("ledger:add_char")
-        self.request.user = AnonymousUser()
-        self.token = Mock()
+        request_factory = RequestFactory()
+        cls.request = request_factory.get("ledger:add_char")
+        cls.request.user = cls.user
+        cls.token = Mock()
 
     @patch("ledger.models.characteraudit.CharacterAudit.objects.update_or_create")
     @patch("allianceauth.eveonline.models.EveCharacter.objects.get_character_by_id")
@@ -48,31 +60,41 @@ class AddCharTest(TestCase):
 
 
 class FetchMemberAuditTest(TestCase):
-    def setUp(self):
-        # Erstellen Sie eine Anfrage und ein Character Mock-Objekt
-        self.factory = RequestFactory()
-        self.request = self.factory.get("ledger:ledger_fetch_memberaudit")
-        self.request.user = AnonymousUser()
-        self.character = Mock()
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserMainFactory(
+            permissions=[
+                "ledger.basic_access",
+            ]
+        )
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        # Erstellen Sie eine Anfrage und ein Token Mock-Objekt
+        request_factory = RequestFactory()
+        cls.request = request_factory.get("ledger:ledger_fetch_memberaudit")
+        cls.request.user = cls.user
+        cls.character = Mock()
 
     @patch("ledger.models.characteraudit.CharacterAudit.objects.get_or_create")
-    @patch("ledger.task.update_character.apply_async")
+    @patch("ledger.tasks.update_character.apply_async")
     @patch("django.contrib.messages.info")
     @patch("django.contrib.messages.error")
     @patch("memberaudit.models.Character.objects.filter")
     def test_fetch_memberaudit(
-        self, mock_filter, mock_error, mock_info, mock_apply_async, mock_get_or_create
+        cls, mock_filter, mock_error, mock_info, mock_apply_async, mock_get_or_create
     ):
         # Setzen Sie die Rückgabewerte der Mock-Objekte
-        self.character.eve_character.character_id = "123"
-        mock_filter.return_value = [self.character]
+        cls.character.eve_character.character_id = "123"
+        mock_filter.return_value = [cls.character]
 
         # Rufen Sie die Funktion auf
-        response = fetch_memberaudit(self.request)
+        response = fetch_memberaudit(cls.request)
 
         # Überprüfen Sie, ob die Mock-Objekte aufgerufen wurden
         mock_get_or_create.assert_called_once_with(
-            character=self.character.eve_character, id=self.character.id
+            character=cls.character.eve_character, id=cls.character.id
         )
         mock_apply_async.assert_called_once_with(
             args=["123"], kwargs={"force_refresh": True}, priority=6
@@ -80,5 +102,5 @@ class FetchMemberAuditTest(TestCase):
         mock_info.assert_called_once()
 
         # Überprüfen Sie die Antwort
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "ledger/index.html")
+        cls.assertEqual(response.status_code, 200)
+        cls.assertTemplateUsed(response, "ledger/index.html")
