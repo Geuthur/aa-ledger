559fc6f51b35ee4d78651b73a4b04a29cc367221
Geuthur <761682+Geuthur@users.noreply.github.com>
2024-05-27 20:19:10 +0200

    #

--- ledger/tests/test_views.py
+++ ledger/tests/test_views.py
@@ -17,4 +17,4 @@ class TestViews(TestCase):
         request.user = self.user
         response = ledger_index(request)
         self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertEqual(response.url, reverse("freight:ledger_char_index"))
+        self.assertEqual(response.url, reverse("ledger:ledger_char_index"))
