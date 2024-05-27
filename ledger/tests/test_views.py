50456b4414610e236b9ea1785fc5f4fe0444ab94
Geuthur <761682+Geuthur@users.noreply.github.com>
2024-05-27 20:36:06 +0200

    #

--- ledger/tests/test_views.py
+++ ledger/tests/test_views.py
@@ -3,6 +3,8 @@ from http import HTTPStatus
 from django.test import RequestFactory, TestCase
 from django.urls import reverse
 
+from app_utils.testdata_factories import UserMainFactory
+
 from ledger.views.pve import ledger_index
 
 
@@ -11,6 +13,7 @@ class TestViews(TestCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.factory = RequestFactory()
+        cls.user = UserMainFactory()
 
     def test_view(self):
         request = self.factory.get(reverse("ledger:index"))
