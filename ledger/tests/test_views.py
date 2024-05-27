7cbe87cb9c84ecac0fd43f17602cb0f59c0a17ad
Geuthur <761682+Geuthur@users.noreply.github.com>
2024-05-27 20:50:58 +0200

    #

--- ledger/tests/test_views.py
+++ ledger/tests/test_views.py
@@ -1,10 +1,13 @@
 from http import HTTPStatus
 
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 from django.test import RequestFactory, TestCase
 from django.urls import reverse
 
 from app_utils.testdata_factories import UserMainFactory
 
+from ledger.models import General
 from ledger.views.pve import ledger_index
 
 
@@ -14,6 +17,12 @@ class TestViews(TestCase):
         super().setUpClass()
         cls.factory = RequestFactory()
         cls.user = UserMainFactory()
+
+        content_type = ContentType.objects.get_for_model(General)
+        permission = Permission.objects.get(
+            codename="basic_access", content_type=content_type
+        )
+        cls.user.user_permissions.add(permission)
 
     def test_view(self):
         request = self.factory.get(reverse("ledger:index"))
