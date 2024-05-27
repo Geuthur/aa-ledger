d25c697e1da6e7eb25d6fb0af02e66628d87c19a
Geuthur <761682+Geuthur@users.noreply.github.com>
2024-05-27 19:21:22 +0200

    #

---
+++ ledger/tests/test_views.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+from django.shortcuts import render
+
+
+def test_view(request):
+    context = {
+        "memberaudit": settings.LEDGER_MEMBERAUDIT_USE,
+    }
+    return render(request, "ledger/corpledger/corp_ledger.html", context=context)
