ee5c051b198c85aeb67c5c47318d0dcba6881cf1
Geuthur <geuthur@users.noreply.github.com>
2024-05-28 12:25:02 +0200

    #

--- ledger/tests/test_char_index.py
+++ ledger/tests/test_char_index.py
@@ -4,26 +4,34 @@ from django.contrib.sessions.middleware import SessionMiddleware
 from django.test import RequestFactory, TestCase
 from django.urls import reverse
 
+from allianceauth.eveonline.models import EveCharacter
+from allianceauth.tests.auth_utils import AuthUtils
 from app_utils.testdata_factories import UserMainFactory
 
-from ledger.views.character.char_audit import add_char, fetch_memberaudit
+from ledger.models.characteraudit import CharacterAudit
+from ledger.tasks import update_character
 
 
 class AddCharTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.user = UserMainFactory(
-            permissions=[
-                "ledger.basic_access",
-            ]
+        cls.user1 = AuthUtils.create_user("User1")
+
+        char1 = EveCharacter.object.create(
+            character_id=1,
+            character_name="character.name1",
+            corporation_id=1337,
+            corporation_name="corporation.name1",
+            corporation_ticker="ABC",
         )
 
-    def test_add_char(self):
-        self.client.force_login(self.user)
+        CharacterAudit.objects.update_or_create(
+            character=EveCharacter.objects.get_character_by_id(char1.character_id)
+        )
 
-        response = self.client.get(reverse("ledger:ledger_add_char"))
-
-        self.assertEqual(response.status_code, 302)
+        update_character.apply_async(
+            args=[char1.character_id], kwargs={"force_refresh": True}, priority=6
+        )
 
 
 class FetchMemberAuditTest(TestCase):
