37d92e7da5ff4728b231b4c7b54b3d1066dc9c58
Geuthur <761682+Geuthur@users.noreply.github.com>
2024-05-28 17:06:57 +0200

    Add testdata with allianceauth

--- ledger/tests/test_char_index.py
+++ ledger/tests/test_char_index.py
@@ -3,45 +3,47 @@ from unittest.mock import Mock, patch
 from django.contrib.sessions.middleware import SessionMiddleware
 from django.test import RequestFactory, TestCase
 from django.urls import reverse
+from esi.models import Token
 
-from app_utils.testdata_factories import UserMainFactory
+from app_utils.testing import create_user_from_evecharacter
 
-from ledger.views.character.char_audit import add_char, fetch_memberaudit
+from ledger.models.characteraudit import CharacterAudit
+from ledger.tests.testdata.load_allianceauth import load_allianceauth
+from ledger.views.character.char_audit import add_char
+
+MODULE_PATH = "ledger.views.character.char_audit"
 
 
 class AddCharTest(TestCase):
+
     @classmethod
-    def setUpTestData(cls):
-        cls.user = UserMainFactory(
-            permissions=[
-                "ledger.basic_access",
-            ]
+    def setUpClass(cls):
+        super().setUpClass()
+        load_allianceauth()
+        cls.factory = RequestFactory()
+        cls.user, cls.character_ownership = create_user_from_evecharacter(
+            1001, permissions=["ledger.basic_access"]
         )
 
-    def test_add_char(self):
-        self.client.force_login(self.user)
-
-        response = self.client.get(reverse("ledger:ledger_add_char"), follow=True)
-
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "ledger/index.html")
-
-
-class FetchMemberAuditTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = UserMainFactory(
-            permissions=[
-                "ledger.basic_access",
-            ]
+    @patch(MODULE_PATH + ".messages")
+    @patch(MODULE_PATH + ".update_character")
+    def test_add_char(self, mock_update_character, mock_messages):
+        token = Mock(spec=Token)
+        token.character_id = self.character_ownership.character.character_id
+        request = self.factory.get(reverse("ledger:ledger_add_char"))
+        request.user = self.user
+        request.token = token
+        middleware = SessionMiddleware(Mock())
+        middleware.process_request(request)
+        # given
+        orig_view = add_char.__wrapped__.__wrapped__
+        # when
+        response = orig_view(request, token)
+        # then
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse("ledger:ledger_index"))
+        self.assertTrue(mock_messages.info.called)
+        self.assertTrue(mock_update_character.apply_async.called)
+        self.assertTrue(
+            CharacterAudit.objects.get(character=self.character_ownership.character)
         )
-
-    def test_fetch_memberaudit(self):
-        self.client.force_login(self.user)
-
-        response = self.client.get(
-            reverse("ledger:ledger_fetch_memberaudit"), follow=True
-        )
-
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "ledger/index.html")
