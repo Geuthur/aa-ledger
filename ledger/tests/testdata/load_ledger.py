2fdf1afc9e2ef4b93fdd2c76755e469bcfae0c77
Geuthur <geuthur@users.noreply.github.com>
2024-06-04 13:19:32 +0200

    Missing ledger testdata

--- ledger/tests/testdata/load_ledger.py
+++ ledger/tests/testdata/load_ledger.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 from allianceauth.eveonline.models import EveCharacter, EveCorporationInfo
 
-from ledger.models.characteraudit import CharacterAudit
+from ledger.models.characteraudit import CharacterAudit, CharacterMiningLedger
 from ledger.models.corporationaudit import CorporationAudit
 
 
@@ -32,4 +32,24 @@ def load_corp_audit():
     )
     CorporationAudit.objects.update_or_create(
         corporation=EveCorporationInfo.objects.get(corporation_id=2003)
+    )
+
+
+def load_char_mining():
+    CharacterMiningLedger.objects.all().delete()
+    CharacterMiningLedger(
+        character=CharacterAudit.objects.get(id=1),
+        id="20240316-17425-1001-30004783",
+        date="2024-03-16",
+        type_id=17425,
+        system_id=30004783,
+        quantity=1000,
+    )
+    CharacterMiningLedger(
+        character=CharacterAudit.objects.get(id=1),
+        id="20240316-17423-1001-30004785",
+        date="2024-03-16",
+        type_id=17423,
+        system_id=30004785,
+        quantity=1000,
     )
