3230fd0d1ca3c7b8e37dfe20d71d4a011a34f4e0
Geuthur <geuthur@users.noreply.github.com>
2025-05-06 17:10:51 +0200

    [ADD] Force Refresh Option

--- ledger/views/index.py
+++ ledger/views/index.py
@@ -38,13 +38,20 @@ def admin(request):
         return redirect("ledger:index")
 
     if request.method == "POST":
-        if request.POST.get("run_char_updates"):
-            messages.info(request, _("Queued Update All Characters"))
-            update_all_characters.apply_async(priority=7)
-        if request.POST.get("run_corp_updates"):
-            messages.info(request, _("Queued Update All Corporations"))
-            update_all_corps.apply_async(priority=7)
+        force_refresh = False
+        if request.POST.get("force_refresh", False):
+            force_refresh = True
         if request.POST.get("run_clear_etag"):
             messages.info(request, _("Queued Clear All ETags"))
             clear_all_etags.apply_async(priority=1)
+        if request.POST.get("run_char_updates"):
+            messages.info(request, _("Queued Update All Characters"))
+            update_all_characters.apply_async(
+                kwargs={"force_refresh": force_refresh}, priority=7
+            )
+        if request.POST.get("run_corp_updates"):
+            messages.info(request, _("Queued Update All Corporations"))
+            update_all_corps.apply_async(
+                kwargs={"force_refresh": force_refresh}, priority=7
+            )
     return render(request, "ledger/admin.html")
