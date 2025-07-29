a5dcfed5c68728e83723f1eb860c2a4bf1f1df5c
Geuthur <geuthur@users.noreply.github.com>
2025-07-29 10:50:40 +0200

    [ADD] Character Journal Annoations & Aggregates

--- ledger/tests/test_managers/test_character_journal_manager.py
+++ ledger/tests/test_managers/test_character_journal_manager.py
@@ -2,7 +2,7 @@
 from unittest.mock import patch
 
 # Django
-from django.test import override_settings
+from django.test import TestCase, override_settings
 from django.utils import timezone
 
 # Alliance Auth (External Libs)
@@ -89,3 +89,284 @@ class TestCharacterJournalManager(NoSocketsTestCase):
 
         obj = self.audit.ledger_character_journal.get(entry_id=16)
         self.assertEqual(obj.amount, 10000)
+
+
+class TestCharacterJournalManagerAnnotations(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        load_allianceauth()
+        load_eveuniverse()
+        load_eveentity()
+        cls.audit = create_characteraudit_from_evecharacter(1001)
+
+        cls.eve_character_first_party = EveEntity.objects.get(eve_id=1001)
+        cls.eve_character_second_party = EveEntity.objects.get(eve_id=1002)
+
+        cls.journal_entry = create_wallet_journal_entry(
+            journal_type="character",
+            character=cls.audit,
+            context_id=1,
+            entry_id=10,
+            amount=1000,
+            balance=2000,
+            date=timezone.datetime.replace(
+                timezone.now(),
+                year=2016,
+                month=10,
+                day=29,
+                hour=0,
+                minute=0,
+                second=0,
+                microsecond=0,
+            ),
+            description="Test Journal",
+            first_party=cls.eve_character_first_party,
+            second_party=cls.eve_character_second_party,
+            ref_type="player_donation",
+        )
+
+    def test_annotate_bounty_income(self):
+        qs = self.audit.ledger_character_journal.all().annotate_bounty_income()
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "bounty_income"),
+                "Bounty income annotation should be present",
+            )
+            self.assertEqual(obj.bounty_income, 0)
+
+    def test_annotate_mission_income(self):
+        qs = self.audit.ledger_character_journal.all().annotate_mission_income()
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "mission_income"),
+                "Mission income annotation should be present",
+            )
+            self.assertEqual(obj.mission_income, 0)
+
+    def test_annotate_market_income(self):
+        qs = self.audit.ledger_character_journal.all().annotate_market_income()
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "market_income"),
+                "Market income annotation should be present",
+            )
+            self.assertEqual(obj.market_income, 0)
+
+    def test_annotate_incursion_income(self):
+        qs = self.audit.ledger_character_journal.all().annotate_incursion_income()
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "incursion_income"),
+                "Incursion income annotation should be present",
+            )
+            self.assertEqual(obj.incursion_income, 0)
+
+    def test_annotate_contract_income(self):
+        qs = self.audit.ledger_character_journal.all().annotate_contract_income()
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "contract_income"),
+                "Contract income annotation should be present",
+            )
+            self.assertEqual(obj.contract_income, 0)
+
+    def test_annotate_donation_income(self):
+        qs = self.audit.ledger_character_journal.all().annotate_donation_income()
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "donation_income"),
+                "Donation income annotation should be present",
+            )
+            self.assertEqual(obj.donation_income, 1000.00)
+
+    def test_annotate_insurance_income(self):
+        qs = self.audit.ledger_character_journal.all().annotate_insurance_income()
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "insurance_income"),
+                "Insurance income annotation should be present",
+            )
+            self.assertEqual(obj.insurance_income, 0)
+
+    def test_annotate_milestone_income(self):
+        qs = self.audit.ledger_character_journal.all().annotate_milestone_income()
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "milestone_income"),
+                "Milestone income annotation should be present",
+            )
+            self.assertEqual(obj.milestone_income, 0)
+
+    def test_annotate_corporation_income(self):
+        qs = self.audit.ledger_character_journal.all().annotate_corporation_income()
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "corporation_income"),
+                "Corporation income annotation should be present",
+            )
+            self.assertEqual(obj.corporation_income, 0)
+
+    def test_annotate_daily_goal_income(self):
+        qs = self.audit.ledger_character_journal.all().annotate_daily_goal_income()
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "daily_goal_income"),
+                "Daily goal income annotation should be present",
+            )
+            self.assertEqual(obj.daily_goal_income, 0)
+
+    def test_annotate_miscellaneous_income(self):
+        qs = self.audit.ledger_character_journal.all().annotate_miscellaneous()
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "miscellaneous"),
+                "Miscellaneous income annotation should be present",
+            )
+            self.assertEqual(obj.miscellaneous, 1000.00)
+
+    def test_annotate_miscellaneous_with_exclude(self):
+        qs = (
+            self.audit.ledger_character_journal.all().annotate_miscellaneous_with_exclude()
+        )
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "miscellaneous"),
+                "Miscellaneous with exclude annotation should be present",
+            )
+            self.assertEqual(obj.miscellaneous, 1000.00)
+
+    def test_annotate_contract_cost(self):
+        qs = self.audit.ledger_character_journal.all().annotate_contract_cost()
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "contract_cost"),
+                "Contract costs annotation should be present",
+            )
+            self.assertEqual(obj.contract_cost, 0)
+
+    def test_annotate_market_cost(self):
+        qs = self.audit.ledger_character_journal.all().annotate_market_cost()
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "market_cost"),
+                "Market costs annotation should be present",
+            )
+            self.assertEqual(obj.market_cost, 0)
+
+    def test_annotate_asset_cost(self):
+        qs = self.audit.ledger_character_journal.all().annotate_asset_cost()
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "asset_cost"),
+                "Asset costs annotation should be present",
+            )
+            self.assertEqual(obj.asset_cost, 0)
+
+    def test_annotate_traveling_cost(self):
+        qs = self.audit.ledger_character_journal.all().annotate_traveling_cost()
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "traveling_cost"),
+                "Traveling costs annotation should be present",
+            )
+            self.assertEqual(obj.traveling_cost, 0)
+
+    def test_annotate_production_cost(self):
+        qs = self.audit.ledger_character_journal.all().annotate_production_cost()
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "production_cost"),
+                "Production costs annotation should be present",
+            )
+            self.assertEqual(obj.production_cost, 0)
+
+    def test_annotate_skill_cost(self):
+        qs = self.audit.ledger_character_journal.all().annotate_skill_cost()
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "skill_cost"),
+                "Skill costs annotation should be present",
+            )
+            self.assertEqual(obj.skill_cost, 0)
+
+    def test_annotate_insurance_cost(self):
+        qs = self.audit.ledger_character_journal.all().annotate_insurance_cost()
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "insurance_cost"),
+                "Insurance costs annotation should be present",
+            )
+            self.assertEqual(obj.insurance_cost, 0)
+
+    def test_annotate_planetary_cost(self):
+        qs = self.audit.ledger_character_journal.all().annotate_planetary_cost()
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "planetary_cost"),
+                "Planetary costs annotation should be present",
+            )
+            self.assertEqual(obj.planetary_cost, 0)
+
+    def test_annotate_lp_cost(self):
+        qs = self.audit.ledger_character_journal.all().annotate_lp_cost()
+        for obj in qs:
+            self.assertTrue(
+                hasattr(obj, "lp_cost"),
+                "LP costs annotation should be present",
+            )
+            self.assertEqual(obj.lp_cost, 0)
+
+
+class TestCharacterJournalManagerAggregate(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        load_allianceauth()
+        load_eveuniverse()
+        load_eveentity()
+        cls.audit = create_characteraudit_from_evecharacter(1001)
+
+        cls.eve_character_first_party = EveEntity.objects.get(eve_id=1001)
+        cls.eve_character_second_party = EveEntity.objects.get(eve_id=1002)
+
+        cls.journal_entry = create_wallet_journal_entry(
+            journal_type="character",
+            character=cls.audit,
+            context_id=1,
+            entry_id=10,
+            amount=1000,
+            balance=2000,
+            date=timezone.datetime.replace(
+                timezone.now(),
+                year=2016,
+                month=10,
+                day=29,
+                hour=0,
+                minute=0,
+                second=0,
+                microsecond=0,
+            ),
+            description="Test Journal",
+            first_party=cls.eve_character_first_party,
+            second_party=cls.eve_character_second_party,
+            ref_type="player_donation",
+        )
+
+    def test_aggregate_bounty(self):
+        result = self.audit.ledger_character_journal.all().aggregate_bounty()
+        self.assertEqual(result, 0)
+
+    def test_aggregate_costs(self):
+        result = self.audit.ledger_character_journal.all().aggregate_costs()
+        self.assertEqual(result, 0)
+
+    def test_aggregate_miscellaneous(self):
+        result = self.audit.ledger_character_journal.all().aggregate_miscellaneous()
+        self.assertEqual(result, 1000.00)
+
+    def test_aggregate_ref_type(self):
+        result = self.audit.ledger_character_journal.all().aggregate_ref_type(
+            ref_type=["player_donation"], income=True
+        )
+        self.assertEqual(result, 1000.00)
