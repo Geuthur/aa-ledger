1490890bb03158c65c18e0ec87d7b0f20e144c87
Geuthur <761682+Geuthur@users.noreply.github.com>
2025-09-03 00:14:13 +0200

    [CHANGE] Improve Testing Speed (#170)
    Co-authored-by: Geuthur <geuthur@users.noreply.github.com>

--- testauth/settings/local.py
+++ testauth/settings/local.py
@@ -41,16 +41,18 @@ LOGGING = None
 NOTIFICATIONS_REFRESH_TIME = 30
 NOTIFICATIONS_MAX_PER_USER = 50
 
-# Enter credentials to use MySQL/MariaDB. Comment out to use sqlite3
-DATABASES["default"] = {
-    "ENGINE": "django.db.backends.mysql",
-    "NAME": "temp_allianceauth",
-    "USER": "root",
-    "PASSWORD": "temp_password_aa_tox_tests",
-    "HOST": "127.0.0.1",
-    "PORT": "3306",
-    "OPTIONS": {"charset": "utf8mb4"},
-}
+# Use the USE_MYSQL environment variable to select the database backend (MySQL or Memcached).
+# NOTE: On Windows, set this variable in the system/user environment variables.
+if os.environ.get("USE_MYSQL", True) is True:
+    DATABASES["default"] = {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "temp_allianceauth",
+        "USER": "root",
+        "PASSWORD": "temp_password_aa_tox_tests",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "OPTIONS": {"charset": "utf8mb4"},
+    }
 
 # Add any additional apps to this list.
 INSTALLED_APPS += [
