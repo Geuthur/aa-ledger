3aff5b0440a65ccfe5350dc12b9bdfb02b2088d7
Geuthur <geuthur@users.noreply.github.com>
2025-11-01 10:06:31 +0100

    [ADD] provide `operations` in ESI Client to fit new guidelines for `django-esi` v8

--- ledger/__init__.py
+++ ledger/__init__.py
@@ -5,7 +5,23 @@ __title__ = "Ledger"
 
 __package_name__ = "aa-ledger"
 __app_name__ = "ledger"
-__esi_compatibility_date__ = "2025-08-26"
+__esi_compatibility_date__ = "2025-09-30"
 __app_name_useragent__ = "AA-Ledger"
 
 __github_url__ = f"https://github.com/Geuthur/{__package_name__}"
+
+__character_operations__ = [
+    "GetCharactersCharacterIdWalletJournal",
+    "GetCharactersCharacterIdRoles",
+    "GetCharactersCharacterIdPlanets",
+    "GetCharactersCharacterIdPlanetsPlanetId",
+    "GetCharactersCharacterIdMining",
+]
+
+__corporation_operations__ = [
+    "GetCorporationsCorporationIdWallets",
+    "GetCorporationsCorporationIdWalletsDivisionJournal",
+    "GetCorporationsCorporationIdDivisions",
+]
+
+__universe_operations__ = ["PostUniverseNames"]
